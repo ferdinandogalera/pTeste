bde517a673fabe3cbd2645d08d978facca5fdbc9
Ferdinando <ferdinandogalera@gmail.com>
2021-12-16 08:58:55 -0300

    Salvo Solictacao Senha

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -96,32 +96,5 @@ class HomeController extends Controller
         return ["statuscode" => $existis, "hora" => $horario2];
     }
 
-    public function avaliarCron() {
-        dd("deu certo");
-        $data = taburls::select("id","description")
-        ->orderBy("created_at","desc")
-        ->get();
-        $horario = date("Y-m-d H:i:s");
-        $horario2 = date("d-m-Y H:i:s");
-        foreach ($data as $k => $v) { 
-            $b = str_replace("\r\n","",$v['description']);
-            $www = substr($b,0,3);
-            if ($www!="www" and $www!="htt") {
-                $b = "http://www.".$b;
-            }   
-            $url = parse_url( $b ) ;
-            if (!isset($url['scheme'])) {
-                if ($www!="htt") {
-                   $b = "http://".$b;
-                }
-            }   
-            $file_headers = @get_headers($b);
-            $exists = (!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found'|| $file_headers === false) ? 0 : 1;
-            $existis = ($exists=="0") ? "404" : "200";
-            taburls::where('id', "=", $v['id'])
-                    ->update(['updated_at' => $horario, 'statuscode' => $existis]);
-        }    
-        return view("avaliarCron");
-    }
-   
+    
 }
